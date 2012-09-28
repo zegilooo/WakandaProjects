c10b4a66290f5f738b2833cbed8557ba51145c01
Anas BIHI <anas@iMac-de-Anas-2.local>
2012-09-28 18:26:09 +0100

    update request.waPage

---
+++ Wakaconges/conges/WebFolder/request.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		$$('dialog1').displayDialog();
+		
+				
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
