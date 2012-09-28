246fba5b39b42e008ea1250bf3d04ab88157f10b
Anas BIHI <anas@iMac-de-Anas-2.local>
2012-09-28 16:53:58 +0100

    modification de request.waPage: - ajout du login dialog et des champs pour l'utilisateur connecté

--- Wakaconges/conges/WebFolder/addEmployee.waPage/scripts/index.js
+++ Wakaconges/conges/WebFolder/addEmployee.waPage/scripts/index.js
@@ -2,10 +2,16 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var combobox1 = {};	// @combobox
 	var image1 = {};	// @image
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	combobox1.change = function combobox1_change (event)// @startlock
+	{// @endlock
+		$$('textField8').setValue($$('combobox1').getValue());
+	};// @lock
 
 	image1.click = function image1_click (event)// @startlock
 	{// @endlock
@@ -13,6 +19,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("combobox1", "change", combobox1.change, "WAF");
 	WAF.addListener("image1", "click", image1.click, "WAF");
 // @endregion
 };// @endlock
