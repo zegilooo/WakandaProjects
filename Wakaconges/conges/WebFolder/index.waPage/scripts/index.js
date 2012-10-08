0dfe6cb604d4faeb58533ab61cdfa8018bfcccc6
Anas BIHI <anas@iMac-de-Anas-2.local>
2012-10-08 19:46:02 +0000

    + mise à jour de la page index.waPage + ajout du directory login listner

---
+++ Wakaconges/conges/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,27 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+var component = $$('component1');
+
+// @region namespaceDeclaration// @startlock
+	var dataGrid1 = {};	// @dataGrid
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	dataGrid1.onRowClick = function dataGrid1_onRowClick (event)// @startlock
+	{// @endlock
+		component.loadComponent('/addEmp.waComponent');
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		sources.employees.addNewElement();
+		component.loadComponent('/addEmp.waComponent');
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("dataGrid1", "onRowClick", dataGrid1.onRowClick, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
