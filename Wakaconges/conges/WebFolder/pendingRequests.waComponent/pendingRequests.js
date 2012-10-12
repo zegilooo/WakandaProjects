4d6f93c250bed9691eb1050a1307f409998b73b6
Anas BIHI <anas@iMac-de-Anas-2.local>
2012-10-12 11:59:12 +0000

    Add request approval system

--- Wakaconges/conges/WebFolder/pendingRequests.waComponent/pendingRequests.js
+++ Wakaconges/conges/WebFolder/pendingRequests.waComponent/pendingRequests.js
@@ -13,11 +13,25 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var button1 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		sources.leaves0.approved = false;
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		sources.leaves0.approved = true;
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
+	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
