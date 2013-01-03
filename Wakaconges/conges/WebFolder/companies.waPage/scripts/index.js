dd8fdfab9098fe8c149e64f25a0a9a580b5c5d0b
Anas BIHI <anas@iMac-de-Anas-2.local>
2013-01-03 11:15:54 +0000

    ui optimisation

---
+++ Wakaconges/conges/WebFolder/companies.waPage/scripts/index.js
@@ -0,0 +1,23 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var dataGrid1 = {};	// @dataGrid
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	dataGrid1.onRowClick = function dataGrid1_onRowClick (event)// @startlock
+	{// @endlock
+		if(waf.directory.currentUser()){
+				sessionStorage.currentCompany = sources.company.ID;
+				redirect("/mainpage/");
+			}
+	};// @lock
+	function redirect(path){
+		location.href =path;
+	}
+// @region eventManager// @startlock
+	WAF.addListener("dataGrid1", "onRowClick", dataGrid1.onRowClick, "WAF");
+// @endregion
+};// @endlock
